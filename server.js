6b59fcc08526b88ce6a1d854d573f2fdd38b9fbd
Eric Flores <ericjoseph.flores1@gmail.com>
2018-09-26 11:57:25 +0800

    Pushing server ready

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 4000;
 
 var app = express();
 
@@ -64,6 +65,6 @@ app.get('/bad', (req, res) => {
     });
 })
 
-app.listen(4000, ()=>{
-    console.log('Server is up on port 4000');
+app.listen(port, ()=>{
+    console.log(`Server is up on port ${port}`);
 });
